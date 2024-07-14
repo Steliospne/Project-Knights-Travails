b300760c288ae0dd9253925fbd67bf30f80b1fdb
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-07-14 10:05:57 +0200

    Creates the chessboard visuals + axis labels

--- src/DOM.js
+++ src/DOM.js
@@ -1,29 +1,45 @@
 const Board = require("./board");
 
-module.exports = function initDom() {
-  const boardContainer = document.createElement("div");
-  boardContainer.className = "board";
-  // boardContainer.style.gridTemplateAreas = 
-  // `
-  // "70 71 72 73 74 75 76 77"
-  // "60 61 62 63 64 65 66 67"
-  // "50 51 52 53 54 55 56 57"
-  // "40 41 42 43 44 45 46 47"
-  // "30 31 32 33 34 35 36 37"
-  // "20 21 22 23 24 25 26 27"
-  // "10 11 12 13 14 15 16 17"
-  // "00 01 02 03 04 05 06 07"
-  // `;
-  document.body.append(boardContainer);
+module.exports = class DOM {
+  static init() {
+    const boardContainer = document.createElement("div");
+    boardContainer.className = "board";
 
+    const labelXcontainer = document.createElement("div");
+    labelXcontainer.className = "labelX";
 
-  Board.cells.forEach((row) => {
-    row.forEach((node)=> {
-      const tile = document.createElement("div");
-      tile.className = "node " + node.x + node.y
-      tile.textContent = node.x + " " + node.y
-      tile.style.gridArea = "n"+ node.x + "" + node.y
-      boardContainer.prepend(tile);
-    })
-  });
+    const labelYcontainer = document.createElement("div");
+    labelYcontainer.className = "labelY";
+
+    document.body.append(boardContainer, labelXcontainer, labelYcontainer);
+
+    Board.cells.forEach((row) => {
+      row.forEach((node) => {
+        const tile = document.createElement("div");
+        tile.className = "node " + node.x + node.y;
+        // tile.textContent = node.x + " " + node.y;
+        tile.style.gridArea = "n" + node.x + "" + node.y;
+        boardContainer.prepend(tile);
+
+        if (node.x % 2 == 0) {
+          if (node.y % 2 == 0) {
+            tile.style.backgroundColor = "black";
+          }
+        } else {
+          if (!(node.y % 2 == 0)) {
+            tile.style.backgroundColor = "black";
+          }
+        }
+      });
+    });
+
+    for (let i = 0; i < 8; i++) {
+      const numberX = document.createElement("div");
+      const numberY = document.createElement("div");
+      numberX.className = numberY.className = "node";
+      numberX.textContent = numberY.textContent = i;
+      labelXcontainer.append(numberX);
+      labelYcontainer.prepend(numberY);
+    }
+  }
 };
