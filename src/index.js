d2c1a6b87edd88e86d7835e878b8b3a257ca0a98
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-07-14 10:04:37 +0200

    Initialization of the DOM object

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const DOM = require('./DOM.js');
 const Knight = require('./knight.js');
 
 Board.init()
-DOM()
+DOM.init()
 const knight = new Knight(0,0)
 const target = Board.cells[7][7];
 console.log(knight.moveKnight(knight ,target));
