143ced5f91c55f6b77962696c2a85ac667996236
Stelios <63586679+Steliospne@users.noreply.github.com>
2024-07-16 11:53:04 +0200

    Creates the initUI()
    initUI()
    Renders the UI for setting the start and
    end position. And two btns for starting and
    stopping the animation

--- src/DOM.js
+++ src/DOM.js
@@ -86,5 +86,75 @@ module.exports = class DOM {
     return knightEl;
   }
 
+  static initUI() {
+    const uiWrapper = document.createElement("div");
+    uiWrapper.className = "ui-wrapper";
+
+    const inputWrapper = document.createElement("div");
+    inputWrapper.className = "input-wrapper";
+
+    const startLabel = document.createElement("label");
+    startLabel.textContent = "Set Knight";
+    startLabel.setAttribute("for", "start");
+
+    const startInput = document.createElement("input");
+    startInput.setAttribute("type", "text");
+    startInput.setAttribute("id", "start");
+    startInput.setAttribute("name", "start");
+    startInput.setAttribute("maxlength", "2");
+    startInput.value = "00";
+
+    const targetLabel = document.createElement("label");
+    targetLabel.textContent = "Set Target";
+    targetLabel.setAttribute("for", "target");
+
+    const targetInput = document.createElement("input");
+    targetInput.setAttribute("type", "text");
+    targetInput.setAttribute("id", "target");
+    targetInput.setAttribute("name", "target");
+    targetInput.setAttribute("maxlength", "2");
+
+    inputWrapper.append(startLabel, startInput, targetLabel, targetInput);
+
+    const startAnimationWrapper = document.createElement("span");
+    startAnimationWrapper.className = "su_button_circle";
+
+    const startInnerWrapper = document.createElement("a");
+    startInnerWrapper.className = "button_su_inner";
+
+    const startTextWrapper = document.createElement("span");
+    startTextWrapper.className = "button_text_container";
+
+    const startButton = document.createElement("button");
+    startButton.className = "start button_su";
+    startTextWrapper.textContent = "Start";
+    startButton.addEventListener("click", this.start);
+
+    const stopAnimationWrapper = document.createElement("span");
+    stopAnimationWrapper.className = "su_button_circle";
+
+    const stopInnerWrapper = document.createElement("a");
+    stopInnerWrapper.className = "button_su_inner";
+
+    const stopTextWrapper = document.createElement("span");
+    stopTextWrapper.className = "button_text_container";
+
+    const stopButton = document.createElement("button");
+    stopButton.className = "stop button_su";
+    stopTextWrapper.textContent = "Stop";
+    stopButton.addEventListener("click", this.stop);
+
+    startInnerWrapper.append(startTextWrapper);
+    startButton.append(startAnimationWrapper, startInnerWrapper);
+
+    stopInnerWrapper.append(stopTextWrapper);
+    stopButton.append(stopAnimationWrapper, stopInnerWrapper);
+
+    uiWrapper.append(inputWrapper, startButton, stopButton);
+    document.querySelector(".main").append(uiWrapper);
+
+    mouseAnimation();
+  }
+
   }
 };
